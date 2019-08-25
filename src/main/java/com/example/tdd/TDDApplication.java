1e59ecfdcdee84fd8d4d9d829cae474ba5c07c21
shailendralohia <shailendra_lohia@yahoo.com>
2019-08-25 12:55:15 -0500

    added main file

---
+++ src/main/java/com/example/tdd/TDDApplication.java
@@ -0,0 +1,12 @@
+package com.example.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDDApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TDDApplication.class,args);
+
+    }
+}
